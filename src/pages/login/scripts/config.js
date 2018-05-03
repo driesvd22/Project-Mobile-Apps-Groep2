052e8db1a091d9764ea50d81bf21267b653d1c68
Dries Van Dievoort <driesvandievoort8@gmail.com>
2018-05-03 17:53:55 +0200

    Login via Azure basis

---
+++ src/pages/login/scripts/config.js
@@ -0,0 +1,12 @@
+/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+var APPLICATION_CONFIG = {
+    clientID: "95febb30-6b2a-4d20-8d46-47661c12318d",
+    redirectUri: "http://localhost:8080/",
+    interactionMode: "popUp",
+    graphEndpoint: "https://graph.microsoft.com/v1.0/me",
+    graphScopes: ["user.read mail.send"]
+};
